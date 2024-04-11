ef1c683259d5f3a61d6ed4a7fc8284350c8de7ee
Frank Jäkel-Försterling <jaekel.frank@gmx.de>
2024-04-11 14:55:25 +0200

    fixes

---
+++ quotes.js
@@ -0,0 +1,94 @@
+const randomQuotes = [
+  {
+    quote:
+      '"Frage nicht, was dein Land für dich tun kann, frage was du für dein Land tun kannst."',
+    author: "Kim Jong-il",
+  },
+  {
+    quote:
+      '"Willst du den Charakter eines Menschen erkennen, so gib ihm Macht."',
+    author: "Roland Koch",
+  },
+  {
+    quote: '"Mister Gorbatschow, tear down this wall!"',
+    author: "David Hasselhoff",
+  },
+  {
+    quote: '"Dies ist mein Leib, der für euch hingegeben wird."',
+    author: "Gina Wild",
+  },
+  {
+    quote: '"Ich denke, also bin ich."',
+    author: "Til Schweiger",
+  },
+  {
+    quote: '"Hasta la victoria siempre!"',
+    author: "John D. Rockefeller",
+  },
+  {
+    quote:
+      '"Da hat das rote Pferd sich einfach umgekehrt und hat mit seinem Schwanz die Fliege abgewehrt."',
+    author: "Johann Wolfgang von Goethe",
+  },
+  {
+    quote:
+      '"Herr: es ist Zeit. Der Sommer war sehr groß. Leg deinen Schatten auf die Sonnenuhren, und auf den Fluren lass die Winde los."',
+    author: "H. P. Baxxter",
+  },
+  {
+    quote:
+      '"Der Vorteil der Klugheit besteht darin, dass man sich dumm stellen kann. Das Gegenteil ist schon schwieriger."',
+    author: "Bastian Schweinsteiger",
+  },
+  {
+    quote: '"How much is the fish?"',
+    author: "Karl Marx",
+  },
+  {
+    quote: '"Mailand oder Madrid, hauptsache Italien."',
+    author: "Benedikt der Sechzehnte",
+  },
+  {
+    quote: '"Wenn man ein 0:2 kassiert, dann ist ein 1:1 nicht mehr möglich."',
+    author: "Satz des Pythagoras",
+  },
+  {
+    quote: '"Liebe deinen Nächsten wie dich selbst."',
+    author: "Kurt Cobain",
+  },
+  {
+    quote:
+      '"Handle nur nach derjenigen Maxime, durch die du zugleich wollen kannst, dass sie ein allgemeines Gesetz werde."',
+    author: "Silvio Berlusconi",
+  },
+  {
+    quote: '"Die Proletarier haben nichts zu verlieren als ihre Ketten."',
+    author: "Heidi Klum",
+  },
+  {
+    quote: '"Ihr Völker der Welt, schaut auf diese Stadt!"',
+    author: "Klaus Wowereit",
+  },
+  {
+    quote:
+      '"Schön, die Phönizier haben das Geld erfunden. Aber warum so wenig?"',
+    author: "Buddha",
+  },
+  {
+    quote:
+      '"Die Macht ist nicht etwas, was man erwirbt, wegnimmt, teilt, was man bewahrt oder verliert; die Macht ist etwas, was sich von unzähligen Punkten aus und im Spiel ungleicher und beweglicher Beziehungen vollzieht."',
+    author: "Darth Vader",
+  },
+  {
+    quote: '"Niemand hat die Absicht, eine Mauer zu errichten."',
+    author: "Bob der Baumeister",
+  },
+  {
+    quote: '"Macht kaputt, was euch kaputt macht!"',
+    author: "Axel Springer",
+  },
+  {
+    quote: '"Jedes Publikum kriegt die Vorstellung, die es v"',
+    author: "Mario Barth",
+  },
+];
